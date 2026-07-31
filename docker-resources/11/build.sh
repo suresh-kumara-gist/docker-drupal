2b66a7309848627dfa90c4ea00a1e7bda0bf3740
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-07-31 14:24:52 +0530

    debug

--- docker-resources/11/build.sh
+++ docker-resources/11/build.sh
@@ -10,4 +10,4 @@ echo "===> (11) apk add --no-cache mariadb-client git zip"
 
 apk add --no-cache mariadb-client git zip patch
 
-# /docker-resources/11-common/build-step-2.sh
+/docker-resources/11-common/build-step-2.sh
