d1af9672e01435bcc068201aa97e3d9f5bcbcc5a
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-07-31 15:07:52 +0530

    debug

--- docker-resources/11-common/build-step-2.sh
+++ docker-resources/11-common/build-step-2.sh
@@ -7,11 +7,10 @@ set -e
 echo "===> (11) composer install"
 mv /composer-file/composer.json /var/www/html/composer.json
 cd /var/www/html && \
-  composer config --no-plugins allow-plugins.php-http/discovery true
-  # && \
-  # composer install
+  composer config --no-plugins allow-plugins.php-http/discovery true && \
+  composer install
 rm -Rf vendor/drush
-# composer install
+composer install
 # ls -lah /var/www/html
 # cat /var/www/html/core/lib/Drupal.php|grep VERS
 # ln -s /var/www/html/vendor/bin/drush /bin/drush
